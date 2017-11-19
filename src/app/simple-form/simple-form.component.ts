63cacd2557b31eb39833e09dbb0c719316e713db
Michal Zagrodzki <michalzagrodzki@gmail.com>
2017-11-19 17:11:10 +0100

    Stage 11. Remove unnecessary <div> in simple-form.component

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,10 +3,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <div>
-      <input #myInput type="text" [(ngModel)]="message">
-      <button (click)="update.emit({text: message})">Click me!</button>
-    </div>
+    <input #myInput type="text" [(ngModel)]="message">
+    <button (click)="update.emit({text: message})">Click me!</button>
   `,
   styles: []
 })
